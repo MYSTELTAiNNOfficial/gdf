276e92b40b872d840b535600c0abcc164b2b2617
MYSTELTAiNN <MYSTELTAiNNOfficial@users.noreply.github.com>
2021-05-17 10:33:52 +0700

    Change 17-05-2021

--- db-controller.php
+++ db-controller.php
@@ -1,3 +1,19 @@
 <?php
+//connect database
+function my_connectDB(){
+    $host = "localhost";
+    $user = "root";
+    $pwd  = "";
+    $db   = "uas";
+    
+    $conn = mysqli_connect($host, $user, $pwd, $db) or die("Error connect to database");
+
+    return $conn;
+}
+
+// function untuk close connection
+function my_closeDB($conn){
+    mysqli_close($conn);
+}
 
 ?>
