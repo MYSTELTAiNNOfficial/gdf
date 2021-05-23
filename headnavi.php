f6791a9a2f8f47f432e8c913fdd7d1971454364b
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-05-23 09:45:32 +0700

    Fixed Navigation

--- headnavi.php
+++ headnavi.php
@@ -8,28 +8,31 @@ include_once "user-controller.php";
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="headnavi.css">
+    <link rel="stylesheet" href="headnavi.css?v=<?php echo time(); ?>">
 </head>
+<?php
+$folder = "image" . chr(92);
+$destination_path = getcwd() . DIRECTORY_SEPARATOR . $folder;
+?>
 
 <body>
-    <?php
-    $folder = "image" . chr(92);
-    $destination_path = getcwd() . DIRECTORY_SEPARATOR . $folder;
-    ?>
     <div id="header">
-        <?php echo '<img id="logo" src="' . $folder . "G.D-Logo.png" . '"></img>'; ?>
-        <p class="welc">Welcome, Guest!</p>
+        <?php echo '<img src="' . $folder . "G.D-Logo.png" . '"></img>'; ?>
+        <p>Welcome, Guest!</p>
+        <h2>Game Discussion Forum</h2>
     </div>
+
+    <button onclick="openNav()">open</button>
 
     <div id="mySidenav" class="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <h2 href="#">About</h2>
+        <a href="#">About</a>
         <a href="#">Services</a>
         <a href="#">Clients</a>
         <a href="#">Contact</a>
     </div>
     <!-- Use any element to open the sidenav -->
-    <span onclick="openNav()">open</span>
+
 
     <script>
         /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
