d77d35e8683f4cb47539696451429fd1339576ff
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-05-23 08:11:19 +0700

    Update headnavi

--- headnavi.php
+++ headnavi.php
@@ -1,31 +1,51 @@
-<?php 
+<?php
 include_once "admin-controller.php";
 include_once "game-controller.php";
 include_once "user-controller.php";
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html>
+
 <head>
-    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
     <link rel="stylesheet" href="headnavi.css">
 </head>
+
 <body>
-<?php
-$folder = "image" . chr(92);
-$destination_path = getcwd() . DIRECTORY_SEPARATOR . $folder;
-?>
-<div id="header">
-<?php echo '<img id="logo" src="' .$folder."G.D-Logo.png". '"></img>'; ?>
-<h4 id="">Welcome, Guest!</h4>
-</div>
-<div>
-<h3>NAVIGATION</h3> 
-<a href="index.php">READ</a>&nbsp;-&nbsp;
-<a href="adddb.php">CREATE</a>&nbsp;-&nbsp;
-<a href="deletedb.php">DELETE</a>&nbsp;-&nbsp;
-</div>
+    <?php
+    $folder = "image" . chr(92);
+    $destination_path = getcwd() . DIRECTORY_SEPARATOR . $folder;
+    ?>
+    <div id="header">
+        <?php echo '<img id="logo" src="' . $folder . "G.D-Logo.png" . '"></img>'; ?>
+        <p class="welc">Welcome, Guest!</p>
+    </div>
+
+    <div id="mySidenav" class="sidenav">
+        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <h2 href="#">About</h2>
+        <a href="#">Services</a>
+        <a href="#">Clients</a>
+        <a href="#">Contact</a>
+    </div>
+    <!-- Use any element to open the sidenav -->
+    <span onclick="openNav()">open</span>
+
+    <script>
+        /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+        }
+
+        /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+            document.getElementById("main").style.marginLeft = "0";
+            document.body.style.backgroundColor = "white";
+        }
+    </script>
 </body>
+
 </html>
