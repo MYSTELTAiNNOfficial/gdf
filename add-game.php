126f706205013faf4e3f740a33620dbd885e2ef6
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-05-24 09:14:35 +0700

    Fix game.css

--- add-game.php
+++ add-game.php
@@ -1,6 +1,7 @@
 <?php include_once("headnavi.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,22 +9,23 @@
     <link rel="stylesheet" href="game.css?v=<?php echo time(); ?>">
     <title>Create Data</title>
 </head>
-<body>
-<div id="formCreate">
-    <h1>Create New Gamelist Data</h1>
 
-    <form action="addgameprocess.php" method="POST" enctype = "multipart/form-data">
-        <label for="gametitle"><b>Game Title</b></label>
-            <br>
-            <input type="text" placeholder="Enter Title" name="name" required>
-            <br>
-            <label for="image"><b>Game Cover Image</b></label>
-            <br>
-            <input type="file"  name="img" accept="img/*" required>
-            <br><br>
-            <input type="submit" value="Upload"> 
-    </form>
-    <button onclick="location.href='admin-edit.php'">Back to admin control</button>
-</div>
+<body>
+    <div id="formCreate">
+        <h1>Create New Gamelist Data</h1>
+            <form action="addgameprocess.php" method="POST" enctype="multipart/form-data">
+                <label for="gametitle"><b>Game Title</b></label>
+                <br>
+                <input type="text" placeholder="Enter Title" name="name" required>
+                <br>
+                <label for="image"><b>Game Cover Image</b></label>
+                <br>
+                <input type="file" name="img" accept="img/*" required>
+                <br><br>
+                <input type="submit" value="Upload">
+            </form>
+        <button onclick="location.href='admin-edit.php'">Back to admin control</button>
+    </div>
 </body>
+
 </html>
