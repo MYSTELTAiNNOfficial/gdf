71c28e0ed78ae1ef40c322f5492ee804bd32a16e
MYSTELTAiNN <53290732+MYSTELTAiNNOfficial@users.noreply.github.com>
2021-06-03 20:54:06 +0700

    Fix

--- contactus.php
+++ contactus.php
@@ -20,16 +20,16 @@
             <tr>
                 <td><img src="image/photo(joey).jpg"></img></td>
                 <td id="comment">
-                    <pre>
-The name is Joey Wiryawan. I am 19 years old, born in Surabaya, Indonesia. 
-I love to do sports and play video games. Hope you found this website useful to use!!
-You can contact me on :
-
-Discord : JoWI#8899
-IG : @joeywwiryawan
-Github : JoWi2404
-Gmail : joeywiryawan2@gmail.com
-                    </pre>
+                    <p>
+The name is Joey Wiryawan. I am 19 years old, born in Surabaya, Indonesia.<br>
+I love to do sports and play video games. Hope you found this website useful to use!!<br>
+You can contact me on :<br>
+<br>
+Discord : JoWI#8899<br>
+IG : @joeywwiryawan<br>
+Github : JoWi2404<br>
+Gmail : joeywiryawan2@gmail.com<br>
+                    </p>
                 </td>
             </tr>
         </table>
@@ -42,16 +42,16 @@ Gmail : joeywiryawan2@gmail.com
             <tr>
                 <td><img src="image/photo(alvin).jpg"></img></td>
                 <td id="comment">
-                    <pre>
-Hi, my name is Alvin Gary. I am 19 Years old, born in Surabaya, Indonesia.
-I like to listening to music especially J-Pop and Anime song. I also like to read Manga and Light Novel.
-You can reach me on:
-
-Discord: -
-Instagram: alvingary201
-GitHub: AlvinGary
-Gmail: alvingary201@gmail.com
-                    </pre>
+                    <p>
+Hi, my name is Alvin Gary. I am 19 Years old, born in Surabaya, Indonesia.<br>
+I like to listening to music especially J-Pop and Anime song. I also like to read Manga and Light Novel.<br>
+You can reach me on:<br>
+<br>
+Discord: -<br>
+Instagram: alvingary201<br>
+GitHub: AlvinGary<br>
+Gmail: alvingary201@gmail.com<br>
+                    </p>
                 </td>
             </tr>
         </table>
@@ -64,17 +64,17 @@ Gmail: alvingary201@gmail.com
             <tr>
                 <td><img src="image/photo(syam).jpg"></img></td>
                 <td id="comment">
-                    <pre>
-Hello There
-My Name is Syamsuddin Putra Riefli, known as MYSTELTAiNN and Rename* (*in game PSO2)
-My hobby is all related thing to Anime.
-
-You can reach me on:
-Discord: MSYTELTAiNN#6674
-Instagram: mysteltainn.official
-GitHub: MYSTELTAiNNOfficial
-Gmail: -
-                    </pre>
+                    <p>
+Hello There<br>
+My Name is Syamsuddin Putra Riefli, known as MYSTELTAiNN and Rename* (*in game PSO2)<br>
+My hobby is all related thing to Anime.<br>
+<br>
+You can reach me on:<br>
+Discord: MSYTELTAiNN#6674<br>
+Instagram: mysteltainn.official<br>
+GitHub: MYSTELTAiNNOfficial<br>
+Gmail: -<br>
+                    </p>
                 </td>
             </tr>
         </table>
